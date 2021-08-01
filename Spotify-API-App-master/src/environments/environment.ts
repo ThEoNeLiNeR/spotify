1837a6ab3096d0e0d84d88ed686ada33e047da24
ThEoNeLiNeR <the.one.liner.patra@gmail.com>
2021-08-01 14:17:53 +0530

    hello

---
+++ Spotify-API-App-master/src/environments/environment.ts
@@ -0,0 +1,10 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+   clientId: 'b410bd9e934e4687a73ebec766abdaba',
+   clientSecret: '96ff9b7839a942479768d9bffbafb256'
+};
